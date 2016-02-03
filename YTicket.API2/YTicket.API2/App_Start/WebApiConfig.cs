be127c76f44af6cbdf453a8f6f6cb88035430233
HiepBP <phuhiep11395@gmail.com>
2016-02-03 15:51:08 +0700

    Add Register

--- YTicket.API2/YTicket.API2/App_Start/WebApiConfig.cs
+++ YTicket.API2/YTicket.API2/App_Start/WebApiConfig.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace YTicket.API2
 {
@@ -12,6 +14,9 @@ namespace YTicket.API2
     {
         public static void Register(HttpConfiguration config)
         {
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.Re‌​ferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
