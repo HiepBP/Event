53f3fa06fb7065f3473335df63a1de385e4617b9
giathienpham <giathienpham@gmail.com>
2016-02-22 00:28:14 +0700

    thienpg- add function search for event (in progress)

--- Android/YTicket/app/src/main/java/com/fpt/study/yticket/activity/MainActivity.java
+++ Android/YTicket/app/src/main/java/com/fpt/study/yticket/activity/MainActivity.java
@@ -11,9 +11,13 @@ import android.support.v4.app.ListFragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TabHost;
 
 import com.fpt.study.yticket.R;
@@ -27,6 +31,7 @@ import java.util.List;
 public class MainActivity extends FragmentActivity {
 
     TabHost tabHost;
+    EditText mEditTextSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,31 @@ public class MainActivity extends FragmentActivity {
 
         tabHost = (TabHost) findViewById(android.R.id.tabhost);
         tabHost.setup();
+
+        mEditTextSearch = (EditText) findViewById(R.id.activity_main_search);
+        mEditTextSearch.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.requestFocusFromTouch();
+                return false;
+            }
+        });
+        mEditTextSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         TabHost.OnTabChangeListener tabChangeListener = new TabHost.OnTabChangeListener() {
             @Override
@@ -83,13 +113,13 @@ public class MainActivity extends FragmentActivity {
         /** Defining tab builder for Andriod tab */
         TabHost.TabSpec tSpecAndroid = tabHost.newTabSpec("home");
         tSpecAndroid.setContent(new TabContent(getBaseContext()));
-        tSpecAndroid.setIndicator("ALL SHITS", getResources().getDrawable(R.drawable.user_image));
+        tSpecAndroid.setIndicator("New Events", getResources().getDrawable(R.drawable.user_image));
         tabHost.addTab(tSpecAndroid);
 
         /** Defining tab builder for Apple tab */
         TabHost.TabSpec tSpecApple = tabHost.newTabSpec("user");
         tSpecApple.setContent(new TabContent(getBaseContext()));
-        tSpecApple.setIndicator("HOT SHITS", getResources().getDrawable(R.drawable.user_image));
+        tSpecApple.setIndicator("Why User here?", getResources().getDrawable(R.drawable.user_image));
         tabHost.addTab(tSpecApple);
 
     }
